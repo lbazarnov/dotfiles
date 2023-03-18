2f317e43605b8e5a26ff982ddcca483e46e357a9
Lev Bazarnov <16255087+lbazarnov@users.noreply.github.com>
2023-03-18 13:17:22 +0400

    updated config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    rbenv init - fish | source
 end
 
 starship init fish | source
@@ -9,3 +10,4 @@ alias ll="exa -la --git --colour=automatic"
 set -gx MICRO_TRUECOLOR 1
 set PATH /opt/homebrew/sbin $PATH
 set PATH /Users/levbazarnov/.local/bin $PATH
+set PATH /opt/homebrew/opt/node@18/bin $PATH
